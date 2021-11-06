5a3b52726e4e5005fa4a15e3886591aa4bef098e
user <slavad878@gmail.com>
2021-11-06 15:48:05 +0300

    Refactor echo_server.c
    - retrieve system funcs to outside lib file
    - create lib.{h,c}

---
+++ lib.h
@@ -0,0 +1,22 @@
+#ifndef __lib_h
+#define __lib_h
+
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int Socket(int, int, int);
+void Bind(int, const struct sockaddr *, size_t);
+void Listen(int, int);
+int Accept(int, struct sockaddr *, socklen_t *);
+pid_t Fork();
+void Close(int);
+void Writen(int, const void *, size_t);
+
+#endif
