f19f2ac5fbb7d233aa6e83d2326bc9dd418ac847
user <slavad878@gmail.com>
2021-11-06 10:53:07 +0300

    Implementing standard echo server/client
    - clear echo_client.c, empty now
    - rewrite echo_server.c
    - handle errors in lib funcs calls
    - rewrite main server process
    - add writen(..., size_t n) to write n bytes

--- echo_server.c
+++ echo_server.c
@@ -1,4 +1,6 @@
 #include <sys/types.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -6,50 +8,134 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  int sock, listener;
-  struct sockaddr_in addr;
-  char buf[1024];
-  int bytes_read;
+int
+Socket(int family, int type, int protocol) {
+  int ret;
 
-  listener = socket(AF_INET, SOCK_STREAM, 0);
-  if (listener < 0) {
+  if (ret = (socket(family, type, protocol)) < 0) {
     perror("socket");
     exit(1);
   }
+  return ret;
+}
 
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(13);
-  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-  if (bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+void
+Bind(int fd, const struct sockaddr *addr, size_t len) {
+  if (bind(fd, addr, len) < 0) {
     perror("bind");
-    exit(2);
+    exit(1);
   }
+}
 
-  listen(listener, 1);
-  char buff[128];
-  printf("listening on %s, port %d\n",
-    inet_ntop(AF_INET, &addr.sin_addr, buff, sizeof(buff)),
-    ntohs(addr.sin_port));
+void
+Listen(int fd, int backlog) {
+  if (listen(fd, backlog) < 0) {
+    perror("listen");
+    exit(1);
+  }
+}
 
-  while(1) {
-    printf("waiting for connections...\n");
-    sock = accept(listener, NULL, NULL);
-    printf("received new connection\n");
-    if (sock < 0) {
-      perror("accept");
-      exit(3);
+int
+Accept(int fd, struct sockaddr *addr, socklen_t *len) {
+  int ret;
+
+  if ((ret = accept(fd, addr, len)) < 0) {
+    perror("accept");
+    exit(1);
+  }
+  return ret;
+}
+
+pid_t
+Fork() {
+  pid_t p;
+
+  if ((p = fork()) == -1) {
+    perror("fork");
+    exit(1);
+  }
+  return p;
+}
+
+void
+Close(int fd) {
+  if (close(fd) == -1) {
+    perror("close");
+    exit(1);
+  }
+}
+
+ssize_t
+writen(int fd, const void *line, size_t n) {
+  size_t nleft;
+  ssize_t nwritten;
+  const char *vptr;
+
+  vptr = (const char *)line;
+  nleft = n;
+  while (nleft > 0) {
+    if ((nwritten = write(fd, vptr, n)) <= 0) {
+      if (nwritten == -1 && errno == EINTR) { /* a signal interrupted write call */
+        nwritten = 0;
+        continue; /* try again */
+      } else {
+        return -1;
+      }
+    }
+    nleft -= nwritten;
+    vptr += nwritten;
+  }
+  return n;
+}
+
+void
+Writen(int fd, const void *line, size_t n) {
+  if (writen(fd, line, n) != n) {
+    perror("writen");
+    exit(1);
+  }
+}
+
+void
+str_echo(int fd) {
+  static const int MAXLINE = 1024;
+  ssize_t n;
+  char buf[MAXLINE];
+
+  while (1) {
+    if ((n = read(fd, buf, MAXLINE)) == 0) {
+      return;
     }
 
-    while (1) {
-      bytes_read = recv(sock, buf, 1024, 0);
-      if (bytes_read <= 0) break;
-      printf("received %s\n", buf);
-      send(sock, buf, bytes_read, 0);
-    }
-
-    close(sock);
+    Writen(fd, buf, n);
   }
+}
 
-  return 0;
+int
+main(int argc, char **argv) {
+  int listenfd, cfd;
+  pid_t childpid;
+  struct sockaddr_in caddr, saddr;
+  socklen_t clen;
+
+  listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+  memset(&saddr, 0, sizeof(saddr));
+  saddr.sin_family = AF_INET;
+  saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  saddr.sin_port = htons(9877);
+
+  Listen(listenfd, 5);
+
+  while (1) {
+    clen = sizeof(caddr);
+    cfd = Accept(listenfd, (struct sockaddr *)&caddr, &clen);
+
+    if ((childpid = Fork()) == 0) {
+      Close(listenfd);
+      str_echo(cfd);
+      exit(0);
+    }
+    Close(cfd);
+  }
 }
