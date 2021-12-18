3145a42630781299944803080fdce375d3240c09
user <slavad878@gmail.com>
2021-12-18 10:55:16 +0300

    Print rcv/sndbuf sizes for tcp,udp,sctp sockets

--- limits/sobuf.c
+++ limits/sobuf.c
@@ -1,20 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+get_buf_sizes(int fd, int *recvbuf, int *sndbuf) {
+  int rcvlen, sndlen;
+
+  rcvlen = sizeof(*recvbuf);
+  if (getsockopt(fd, SOL_SOCKET, SO_RCVBUF, recvbuf, &rcvlen) == -1) {
+    perror("tcp rcvbuf getsockopt");
+  }
+
+  sndlen = sizeof(*sndbuf);
+  if (getsockopt(fd, SOL_SOCKET, SO_SNDBUF, sndbuf, &sndlen) == -1) {
+    perror("tcp sndbuf getsockopt");
+  }
+}
+
+struct bufsize {
+  int fd;
+  int recvbuf;
+  int sndbuf;
+};
 
 int
 main() {
-  int fd, val, len;
-  fd = socket(AF_INET, SOCK_STREAM, 0);
+  struct bufsize tcpbufsize;
+  struct bufsize udpbufsize;
+  struct bufsize sctpbufsize;
 
-  len = sizeof(val);
-  if (getsockopt(fd, SOL_SOCKET, SO_SNDBUF, &val, &len) == -1) {
-    perror("getsockopt");
-  } else {
-    printf("default SO_SNDBUF = %d\n", val);
-  }
+  tcpbufsize.fd  = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  udpbufsize.fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  sctpbufsize.fd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
 
-  exit(1);
+  get_buf_sizes(tcpbufsize.fd, &tcpbufsize.recvbuf, &tcpbufsize.sndbuf);
+  get_buf_sizes(udpbufsize.fd, &udpbufsize.recvbuf, &udpbufsize.sndbuf);
+  get_buf_sizes(sctpbufsize.fd, &sctpbufsize.recvbuf, &sctpbufsize.sndbuf);
+
+  printf("TCP SO_RCVBUF = %d, SO_SNDBUF = %d\n", tcpbufsize.recvbuf, tcpbufsize.sndbuf);
+  printf("UDP SO_RCVBUF = %d, SO_SNDBUF = %d\n", udpbufsize.recvbuf, udpbufsize.sndbuf);
+  printf("SCTP SO_RCVBUF = %d, SO_SNDBUF = %d\n", sctpbufsize.recvbuf, sctpbufsize.sndbuf);
+
+  exit(0);
 }
